80a09f72f24377817ad5229262527eabeb90816b
Nikolas North <nikolasn09@gmail.com>
2021-06-21 20:11:16 -0400

    implement and test can_sum()

--- test/dp_test.cpp
+++ test/dp_test.cpp
@@ -7,3 +7,17 @@ TEST_CASE("fib()") {
   REQUIRE(fib(8) == 21);
   REQUIRE(fib(50) == 12586269025);  // should finish fairly quickly
 }
+
+TEST_CASE("can_sum()") {
+  std::unordered_map<unsigned int, bool> cache;
+
+  REQUIRE(can_sum(7, std::vector<unsigned int>{2, 3}, cache) == true);
+  cache.clear();
+  REQUIRE(can_sum(7, std::vector<unsigned int>{5, 3, 4, 7}, cache) == true);
+  cache.clear();
+  REQUIRE(can_sum(7, std::vector<unsigned int>{2, 4}, cache) == false);
+  cache.clear();
+  REQUIRE(can_sum(8, std::vector<unsigned int>{2, 3, 5}, cache) == true);
+  cache.clear();
+  REQUIRE(can_sum(300, std::vector<unsigned int>{7, 14}, cache) == false);  // should finish fairly quickly
+}
